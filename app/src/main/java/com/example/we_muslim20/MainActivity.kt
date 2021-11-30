b26afe64f6a7e4acd17c9f208cc9b843c27ac943
Harits <haritsdeveloper@gmail.com>
2021-11-30 10:37:31 +0700

    :sparkles:

--- app/src/main/java/com/example/we_muslim20/MainActivity.kt
+++ app/src/main/java/com/example/we_muslim20/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.we_muslim20
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import com.example.we_muslim20.home.AsmaulHusnaActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var imageButton1 = findViewById<ImageButton>(R.id.img_btn_fitur1)
+
+        var imageButton2 = findViewById<ImageButton>(R.id.img_btn_fitur2)
+        imageButton2.setOnClickListener {
+            startActivity(Intent(this,AsmaulHusnaActivity::class.java))
+        }
+        var imageButton3 = findViewById<ImageButton>(R.id.img_btn_fitur3)
+
+        var imageButton4 = findViewById<ImageButton>(R.id.img_btn_fitur4)
+
     }
 }
---
+++ app/src/main/java/com/example/we_muslim20/home/DzikirActivity.kt
@@ -0,0 +1,12 @@
+package com.example.we_muslim20.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.we_muslim20.R
+
+class DzikirActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dzikir)
+    }
+}
