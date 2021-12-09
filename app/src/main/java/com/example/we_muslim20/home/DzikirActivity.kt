ac19b3cd6888a5d8b374d74676a63869e5d0dd04
Harits <haritsdeveloper@gmail.com>
2021-12-09 15:02:22 +0700

    :racehorse:

--- app/src/main/java/com/example/we_muslim20/home/DzikirActivity.kt
+++ app/src/main/java/com/example/we_muslim20/home/DzikirActivity.kt
@@ -1,15 +1,26 @@
 package com.example.we_muslim20.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 import com.example.we_muslim20.R
+import com.example.we_muslim20.home.DzikirSetelahSholat.DzikirSholatActivity
+import com.google.android.material.card.MaterialCardView
 
 class DzikirActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dzikir)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val pindah = findViewById<MaterialCardView>(R.id.crd_sholat)
+        pindah.setOnClickListener {
+            startActivity(Intent(this, DzikirSholatActivity::class.java))
+        }
+
     }
+
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return super.onSupportNavigateUp()
